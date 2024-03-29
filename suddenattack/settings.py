7e6d06b2c3d9d5f843470d293da90b7e0b61a9d5
ykh9871 <ykh9871@gmail.com>
2024-03-29 22:00:27 +0900

    [Refactor] Add drf-yasg

--- suddenattack/settings.py
+++ suddenattack/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_yasg",
     "user",
     "group",
     "board",
-    "rest_framework",
 ]
 
 MIDDLEWARE = [
