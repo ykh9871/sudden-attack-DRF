95ab8edf7fc510bb3957a39733a572437cb3463b
ykh9871 <ykh9871@gmail.com>
2024-03-29 21:42:29 +0900

    [Feat] Added newly created apps to INSTALLED_APPS

--- suddenattack/settings.py
+++ suddenattack/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api",
+    "user",
+    "group",
+    "board",
     "rest_framework",
 ]
 
