23ca6fe9940014d98f9770ac4b646ec8aac8fece
ykh9871 <ykh9871@gmail.com>
2024-04-07 14:22:55 +0900

    [Feat] Added views for listing categories, boards, board details, board comments, and managing board comments including creation, retrieval, update, and deletion.

--- board/views.py
+++ board/views.py
@@ -1,3 +1,107 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from django.utils import timezone
+from .models import Category, Board, BoardComment, GroupMember
+from .serializers import (
+    CategorySerializer,
+    BoardSerializer,
+    BoardCreateSerializer,
+    BoardCommentSerializer,
+    BoardCommentCreateSerializer,
+)
 
-# Create your views here.
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class BoardListView(generics.ListAPIView):
+    serializer_class = BoardSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        group_id = self.kwargs["group_id"]
+        return Board.objects.filter(group_member__group_id=group_id)
+
+
+class BoardCreateView(generics.CreateAPIView):
+    serializer_class = BoardCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        group_id = self.kwargs["group_id"]
+        try:
+            group_member = GroupMember.objects.get(
+                group_id=group_id, user=self.request.user
+            )
+            serializer.save(group_member=group_member)
+        except GroupMember.DoesNotExist:
+            raise PermissionDenied("You are not a member of this group.")
+
+
+class BoardDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        board = self.get_object()
+        group_member = board.group_member
+        if group_member.user != self.request.user:
+            raise PermissionDenied("You do not have permission to update this board.")
+        serializer.save(modified_at=timezone.now())
+
+    def perform_destroy(self, instance):
+        group_member = instance.group_member
+        if group_member.user != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this board.")
+        instance.delete()
+
+
+class BoardCommentListView(generics.ListAPIView):
+    serializer_class = BoardCommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        board_id = self.kwargs["board_id"]
+        return BoardComment.objects.filter(board_id=board_id)
+
+
+class BoardCommentCreateView(generics.CreateAPIView):
+    serializer_class = BoardCommentCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        board_id = self.kwargs["board_id"]
+        try:
+            board = Board.objects.get(id=board_id)
+            group_member = GroupMember.objects.get(
+                group=board.group_member.group, user=self.request.user
+            )
+            serializer.save(group_member=group_member, board=board)
+        except GroupMember.DoesNotExist:
+            raise PermissionDenied(
+                "You are not a member of the group associated with this board."
+            )
+
+
+class BoardCommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BoardComment.objects.all()
+    serializer_class = BoardCommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        comment = self.get_object()
+        group_member = comment.group_member
+        if group_member.user != self.request.user:
+            raise PermissionDenied("You do not have permission to update this comment.")
+        serializer.save(modified_at=timezone.now())
+
+    def perform_destroy(self, instance):
+        group_member = instance.group_member
+        if group_member.user != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this comment.")
+        instance.delete()
