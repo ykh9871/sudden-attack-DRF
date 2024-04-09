5aef1f11c42365e05ffb998d8a479dec1845846f
ykh9871 <ykh9871@gmail.com>
2024-04-09 18:05:37 +0900

    [Feat] Update models and migrations
    변경된 내용 :
    - Created a new custom user model by inheriting AbstractBaseUser provided by Django
    - Deleted previous migration files within the Migration folder.
    - Updated DB schema using migration files.

--- user/models.py
+++ user/models.py
@@ -1,19 +1,65 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 
 class Occupation(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
 
-class User(models.Model):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        occupation_id = extra_fields.pop("occupation", None)
+        if occupation_id:
+            try:
+                occupation = Occupation.objects.get(id=occupation_id)
+                extra_fields["occupation"] = occupation
+            except Occupation.DoesNotExist:
+                raise ValueError("Invalid occupation ID")
+        else:
+            raise ValueError("Superuser must have an occupation")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, unique=True)
     nickname = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
     occupation = models.ForeignKey(Occupation, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
     modified_at = models.DateTimeField(default=None, null=True)
     deleted_at = models.DateTimeField(default=None, null=True)
     refresh_token = models.CharField(max_length=255, default=None, null=True)
-    active = models.IntegerField(default=1)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "nickname", "occupation"]
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
