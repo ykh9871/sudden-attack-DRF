8cfba4c438815e4f1047be7896b0351b7387d403
ykh9871 <ykh9871@gmail.com>
2024-04-09 18:17:37 +0900

    [Feat] Added @swagger_auto_schema decorator
    변경된 내용:
    - Added @swagger_auto_schema decorator to document API endpoints.

--- user/views.py
+++ user/views.py
@@ -4,13 +4,38 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import authenticate
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .models import User
 from .serializers import UserSerializer, UserInfoSerializer
 
 
 class SignupView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "username": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="사용자 이름"
+                ),
+                "nickname": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="닉네임"
+                ),
+                "email": openapi.Schema(type=openapi.TYPE_STRING, description="이메일"),
+                "password": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="비밀번호"
+                ),
+                "occupation": openapi.Schema(
+                    type=openapi.TYPE_INTEGER, description="직업 ID"
+                ),
+            },
+            required=["username", "nickname", "email", "password", "occupation"],
+        ),
+        responses={201: "User registered successfully"},
+    )
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -23,20 +48,43 @@ class SignupView(APIView):
 
 
 class LoginView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "email": openapi.Schema(type=openapi.TYPE_STRING, description="이메일"),
+                "password": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="비밀번호"
+                ),
+            },
+            required=["email", "password"],
+        ),
+        responses={
+            200: openapi.Response(
+                description="Login successful",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        "refresh": openapi.Schema(type=openapi.TYPE_STRING),
+                        "access": openapi.Schema(type=openapi.TYPE_STRING),
+                    },
+                ),
+            ),
+            401: "Incorrect email or password",
+        },
+    )
     def post(self, request):
         email = request.data.get("email")
         password = request.data.get("password")
         user = authenticate(email=email, password=password)
         if user:
             refresh = RefreshToken.for_user(user)
-            user.refresh_token = str(refresh)
-            user.save()
+            serializer = TokenObtainPairSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
             return Response(
                 {
-                    "access_token": str(refresh.access_token),
-                    "token_type": "bearer",
-                    "nickname": user.nickname,
-                    "refresh_token": str(refresh),
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
                 },
                 status=status.HTTP_200_OK,
             )
@@ -50,6 +98,9 @@ class LoginView(APIView):
 class UserInfoView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        responses={200: UserInfoSerializer},
+    )
     def get(self, request):
         user_info = User.objects.filter(email=request.user.email).first()
         if user_info:
@@ -60,6 +111,24 @@ class UserInfoView(APIView):
                 {"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND
             )
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "username": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="사용자 이름"
+                ),
+                "nickname": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="닉네임"
+                ),
+                "occupation": openapi.Schema(
+                    type=openapi.TYPE_INTEGER, description="직업 ID"
+                ),
+            },
+            required=["username", "nickname", "occupation"],
+        ),
+        responses={200: "User information updated successfully"},
+    )
     def post(self, request):
         serializer = UserInfoSerializer(data=request.data)
         if serializer.is_valid():
@@ -67,7 +136,7 @@ class UserInfoView(APIView):
             if user:
                 user.username = serializer.validated_data["username"]
                 user.nickname = serializer.validated_data["nickname"]
-                user.occupation.name = serializer.validated_data["occupation_name"]
+                user.occupation = serializer.validated_data["occupation"]
                 user.save()
                 return Response(
                     {"message": "User information updated successfully"},
@@ -84,6 +153,24 @@ class UserInfoView(APIView):
 class UpdatePasswordView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "current_password": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="현재 비밀번호"
+                ),
+                "new_password": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="새로운 비밀번호"
+                ),
+            },
+            required=["current_password", "new_password"],
+        ),
+        responses={
+            200: "User password updated successfully",
+            401: "Incorrect current password",
+        },
+    )
     def put(self, request):
         current_password = request.data.get("current_password")
         new_password = request.data.get("new_password")
@@ -105,6 +192,18 @@ class UpdatePasswordView(APIView):
 class WithdrawalView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "password": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="비밀번호"
+                ),
+            },
+            required=["password"],
+        ),
+        responses={200: "User account deleted successfully", 401: "Incorrect password"},
+    )
     def delete(self, request):
         password = request.data.get("password")
         user = authenticate(email=request.user.email, password=password)
